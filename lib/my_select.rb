312634373ce899636c6f2d3314796d39f7394e5d
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-09 02:55:31 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,24 +1,13 @@
-def my_select(collection)
-  i = 0 
-  array = []
-  while i < collection.length 
-  array << yield(collection[i])
-  i += 1 
-  my_select(collection) do |num| 
-    collection.select 
-end
- 
-end 
-
 def my_select(array) 
-count = 0
+
+i = 0
 result = []
 
 while count < array.size
-  if yield(array[count]) == true
-  result.push(array[count])
+  if yield(array[i]) == true
+  result << (array[i])
 end
-  count+=1
+  i += 1
 end
 result
 end
