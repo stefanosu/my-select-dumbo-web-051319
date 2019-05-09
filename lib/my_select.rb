d0613f36aafe765c0d3c93c910c07deeda31ba7e
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-09 02:54:37 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -9,3 +9,16 @@ def my_select(collection)
 end
  
 end 
+
+def my_select(array) 
+count = 0
+result = []
+
+while count < array.size
+  if yield(array[count]) == true
+  result.push(array[count])
+end
+  count+=1
+end
+result
+end
